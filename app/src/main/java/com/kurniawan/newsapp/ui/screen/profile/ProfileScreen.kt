6eb24f6b9acef769a95899c031302c052709e1fa
Kurniawan Saputra <saputrak299@gmail.com>
2025-01-23 08:31:37 +0700

    Updating code in profile screen.

--- app/src/main/java/com/kurniawan/newsapp/ui/screen/profile/ProfileScreen.kt
+++ app/src/main/java/com/kurniawan/newsapp/ui/screen/profile/ProfileScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -17,6 +18,7 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -45,11 +47,12 @@ fun ProfileScreen(navHostController: NavHostController) {
                 .padding(innerPadding)
         ) {
             Image(
-                painter = painterResource(id = R.drawable.avatar),
+                painter = painterResource(id = R.drawable.img_profile),
                 contentDescription = "Profile Image",
                 modifier = Modifier
                     .size(85.dp)
                     .align(Alignment.CenterHorizontally)
+                    .clip(CircleShape),
             )
             Text(
                 text = "Kurniawan Saputra",
